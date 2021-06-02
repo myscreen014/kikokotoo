a0dc28a8b2ac4b8b693183f10d05d1320be12ee2
eric tarillon <eric.tarillon@gmail.com>
2021-06-02 15:22:09 +0200

    Design simple calculator

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,25 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index() {
-        return view('home', ['buttons' => '']);
+        $buttons = [ 
+            ['label' => 'C',        'class' => 'button-clear btn-info'], 
+            ['label' => '&divide;', 'class' => 'btn-light'], 
+            ['label' => '7',        'class' => 'btn-light'], 
+            ['label' => '8',        'class' => 'btn-light'],
+            ['label' => '9',        'class' => 'btn-light'],
+            ['label' => '&times;',  'class' => 'btn-light'],
+            ['label' => '4',        'class' => 'btn-light'],
+            ['label' => '5',        'class' => 'btn-light'],
+            ['label' => '6',        'class' => 'btn-light'], 
+            ['label' => '&minus;',  'class' => 'btn-light'], 
+            ['label' => '1',        'class' => 'btn-light'], 
+            ['label' => '2',        'class' => 'btn-light'], 
+            ['label' => '3',        'class' => 'btn-light'], 
+            ['label' => '&plus;',   'class' => 'btn-light'],
+            ['label' => '0',        'class' => 'button-zero btn-light'], 
+            ['label' => ',',        'class' => 'btn-light'],
+            ['label' => '=',        'class' => 'button-egual btn-info' ]
+        ];
+        return view('home', ['buttons' => $buttons]);
     }
 }
