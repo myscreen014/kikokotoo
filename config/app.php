9e22a0ec3e25349b9d6dfb52437f07bd44c04798
eric tarillon <eric.tarillon@gmail.com>
2021-06-02 13:52:46 +0200

    TwigBridge install and change views in twig files

--- config/app.php
+++ config/app.php
@@ -174,6 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        TwigBridge\ServiceProvider::class,
 
     ],
 
@@ -224,6 +225,7 @@ return [
         'Session' => Illuminate\Support\Facades\Session::class,
         'Storage' => Illuminate\Support\Facades\Storage::class,
         'Str' => Illuminate\Support\Str::class,
+        'Twig' => TwigBridge\Facade\Twig::class,
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
